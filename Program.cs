9be0d1a046d3be860b74394ee7b0966c61f1e59d
alhabsi16 <mxmx8283@gmail.com>
2023-05-31 13:38:14 +0400

    update

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("calculator");
+
+
+
+        Console.WriteLine("Enter The First Number");
+        int a = Convert.ToInt32 (Console.ReadLine());
+        Console.WriteLine("Enter The Second Number");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+
+        
+        Console.WriteLine( a + b );
+        Console.WriteLine( a - b );
+        Console.WriteLine( b - a );
+        Console.WriteLine( a / b );
+        Console.WriteLine( b / a );
+        Console.WriteLine( a * b );
+        Console.WriteLine( a % b );
+        Console.WriteLine( b % a );
+
+
+        // string Ans = (fNum +" "+ sNum);
+
+        //Console.WriteLine("Answer = " + Ans );
+        Console.ReadLine();
+    }
+}
